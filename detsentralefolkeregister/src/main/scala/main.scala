929b8587cb29e01aac7f6a277ef62e34374329cc
e.e d3si9n <eed3si9n@gmail.com>
2011-11-06 18:41:31 -0500

    updated usage.

--- detsentralefolkeregister/src/main/scala/main.scala
+++ detsentralefolkeregister/src/main/scala/main.scala
@@ -2,5 +2,4 @@ import ergogroup._
 
 object Main extends App {
   val service = (new DsfBindings with scalaxb.Soap11Clients with scalaxb.DispatchHttpClients).service
-  println(service.hentHistorikk(None, None, None, None))
 }
